0a84ed3999f7585a207702ad8ace01bdf3f6e700
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2017-12-18 16:47:03 +0100

    Add HTTP stuff

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-
 const restify = require('restify');
+const http = require('http');
 
 function getQueue(req, res, next) {
   res.send('abc');
@@ -8,8 +8,45 @@ function getQueue(req, res, next) {
 }
 
 const server = restify.createServer();
+
+server.pre((req, res, next) => {
+  const cookie = req.header('Cookie');
+  if (cookie) {
+    console.log('Cookies:', cookie);
+  } else {
+    console.log('No cookie!');
+  }
+  next();
+});
+
 server.get('/queue', getQueue);
 
 server.listen(8123, () => {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+const listUrl = 'http://projectweek.uclllabs.be/radio/list.php';
+setInterval(() => {
+  http.get(listUrl, (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+    if (statusCode !== 200) {
+      res.resume();
+      throw new Error(`Request failed: ${statusCode}`);
+    }
+
+    res.setEncoding('utf8');
+    let rawData = '';
+    res.on('data', chunk => rawData += chunk);
+    res.on('end', () => {
+      try {
+        const parsedData = JSON.parse(rawData);
+        console.log(parsedData);
+      } catch (e) {
+        console.error(`Got error while fetching list.php: ${e.message}`);
+      }
+    });
+  }).on('error', (e) => {
+    console.error(`Got error while fetching list.php: ${e.message}`);
+  });
+}, 5000);
